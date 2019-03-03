059345de172ffa2f9874c837bc8ee76dce7b7ee3
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-03 04:15:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var kittens = ["Milo","Otis","Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshit(name);
+}
